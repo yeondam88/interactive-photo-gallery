2c2be11e8e92bf2a266a0e10f959032a26dfa0ca
Yeondam Park <giyen9088@gmail.com>
2016-04-21 23:44:38 -0700

    add data-keywords and edit animation

--- js/app.js
+++ js/app.js
@@ -4,23 +4,23 @@ $(document).ready(function() {
     //Fiter Images by data-keywords
     $('#search').keyup(function() {
 
-        var current_query = $(this).val();
+        var current_query = $(this).val().toLowerCase();
 
 
         if (current_query !== "") {
-            $(".gallery li a").hide("ease", function(){});
+            $(".gallery li a").hide("slow", function(){});
 
             $(".gallery li a").each(function() {
 
                 var current_keyword = $(this).attr('data-keywords');
 
                 if (current_keyword.indexOf(current_query) >= 0) {
-                    $(this).show("ease", function(){});
+                    $(this).show();
                 }
 
             });
         } else {
-            $(".gallery li a").show("ease", function(){});
+            $(".gallery li a").show("slow", function(){});
         }
     });
 
